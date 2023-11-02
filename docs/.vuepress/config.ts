5bfd3f11dbd0dc3751c12a077af97c91d99c2bbd
Nacho <kulujun@gmail.com>
2023-11-02 15:41:56 +0800

    1

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -28,11 +28,15 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
             },
             {
                 text: '安装使用',
-                link: '/wiki/shadowviewer/use/',
+                link: '/wiki/shadowviewer/use/install/',
             },
             {
-                text: '参与开发',
-                link: '/wiki/shadowviewer/develop/',
+                text: '主项目开发',
+                link: '/wiki/shadowviewer/develop/index/',
+            },
+            {
+                text: '插件开发',
+                link: '/wiki/shadowviewer/plugin/index/',
             }
         ],
         sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
