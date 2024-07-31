b674424caac7e747c04b3a861e81ca5a8849e8e5
kitUIN <kulujun@gmail.com>
2024-07-31 17:38:09 +0800

    Support TitleBar Theme Changed

--- ShadowViewer/MainWindow.xaml.cs
+++ ShadowViewer/MainWindow.xaml.cs
@@ -3,13 +3,18 @@ namespace ShadowViewer;
 public sealed partial class MainWindow : Window
 {
     public MainViewModel ViewModel { get; } = DiFactory.Services.Resolve<MainViewModel>();
+    public ICallableService Caller { get; } = DiFactory.Services.Resolve<ICallableService>();
 
     public MainWindow()
     {
         InitializeComponent();
-        AppTitleBar.Window = this;
         SystemBackdrop = new MicaBackdrop();
         Log.Information("Theme:{Theme}", ((FrameworkElement)this.Content).RequestedTheme);
+        Caller.ThemeChangedEvent += AppTitleBar_ThemeChangedEvent;
     }
 
+    private void AppTitleBar_ThemeChangedEvent(object? sender, EventArgs e)
+    {
+        AppTitleBar.InvokeThemeChanged(this);
+    }
 }
