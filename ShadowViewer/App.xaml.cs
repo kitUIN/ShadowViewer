a1c01850eea657fc25c61cdb70e82f34e1793720
kitUIN <kulujun@gmail.com>
2025-02-17 17:19:40 +0800

    Feat | 启动页面初步完成

--- ShadowViewer/App.xaml.cs
+++ ShadowViewer/App.xaml.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using CustomExtensions.WinUI;
 using Microsoft.UI.Xaml;
 using Microsoft.Windows.AppLifecycle;
 using ShadowViewer.Core.Helpers;
-using ShadowViewer.Helpers;
 using System.Diagnostics;
 using Windows.ApplicationModel.Activation;
-using WinUIEx;
 
 namespace ShadowViewer
 {
@@ -14,7 +11,6 @@ namespace ShadowViewer
     {
         public App()
         {
-            ApplicationExtensionHost.Initialize(this);
             this.InitializeComponent();
         }
 
@@ -24,24 +20,20 @@ namespace ShadowViewer
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            var loadingMainWindow = new LoadingWindow(typeof(MainWindow));
-            loadingMainWindow.Completed += (_, startupWindow) =>
+            var firstUri = new Uri("shadow://local/bookshelf");
+            var startupWindow = new MainWindow(firstUri);
+            WindowHelper.TrackWindow(startupWindow);
+            ThemeHelper.Initialize(startupWindow);
+            var actEventArgs = AppInstance.GetCurrent().GetActivatedEventArgs();
+            if (actEventArgs.Kind == ExtendedActivationKind.Protocol
+                && actEventArgs.Data is IProtocolActivatedEventArgs data)
             {
-                WindowHelper.TrackWindow(startupWindow);
-                ThemeHelper.Initialize(startupWindow);
-                var firstUri = new Uri("shadow://local/bookshelf");
-                var actEventArgs = AppInstance.GetCurrent().GetActivatedEventArgs();
-                if (actEventArgs.Kind == ExtendedActivationKind.Protocol
-                    && actEventArgs.Data is IProtocolActivatedEventArgs data)
-                {
-                    firstUri = data.Uri;
-                }
+                firstUri = data.Uri;
+            }
 
-                Debug.WriteLine("启动");
-                startupWindow.Activate();
-                // 导航
-                NavigateHelper.ShadowNavigate(firstUri);
-            };
+            Debug.WriteLine("启动");
+            startupWindow.Activate();
+            // // 导航
         }
     }
 }
--- ShadowViewer/MainWindow.xaml.cs
+++ ShadowViewer/MainWindow.xaml.cs
@@ -5,31 +5,136 @@ using DryIoc;
 using Microsoft.UI.Xaml;
 using ShadowPluginLoader.WinUI;
 using ShadowViewer.ViewModels;
-using Microsoft.UI.Xaml.Media;
-using WinUIEx;
+using Serilog;
+using ShadowViewer.Core.Cache;
+using ShadowViewer.Core.Helpers;
+using ShadowViewer.Core.Models;
+using ShadowViewer.Core;
+using ShadowViewer.Plugin.Local;
+using ShadowViewer.Plugin.PluginManager;
+using ShadowViewer.Services;
+using SqlSugar;
+using System.Globalization;
+using System.Numerics;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Controls;
+using ShadowViewer.Pages;
+using CommunityToolkit.WinUI.Animations;
+using CustomExtensions.WinUI;
+using ShadowViewer.Helpers;
 
 namespace ShadowViewer;
 
 public sealed partial class MainWindow : Window
 {
-    public MainViewModel ViewModel { get; } = DiFactory.Services.Resolve<MainViewModel>();
-    public ICallableService Caller { get; } = DiFactory.Services.Resolve<ICallableService>();
-
+    public MainViewModel ViewModel { get; set; }
+    private NavigationPage? navigationPage;
+    private readonly Uri? firstUri;
     public MainWindow()
     {
         InitializeComponent();
-        SystemBackdrop = new MicaBackdrop();
-        Caller.ThemeChangedEvent += AppTitleBar_ThemeChangedEvent;
     }
 
+    public MainWindow(Uri firstUri) : this()
+    {
+        this.firstUri = firstUri;
+    }
 
     private void AppTitleBar_ThemeChangedEvent(object? sender, EventArgs e)
     {
         AppTitleBar.InvokeThemeChanged(this);
     }
 
-    private async void FrameworkElement_OnLoaded(object sender, RoutedEventArgs e)
+    private async void Content_Loaded(object sender, RoutedEventArgs e)
     {
+        await AnimationBuilder.Create()
+            .Translation(
+                from: new Vector2(0, 255),
+                to: new Vector2(0, 0))
+            .Opacity(
+                from: 0,
+                to: 1.0,
+                duration: TimeSpan.FromSeconds(0.5))
+            .StartAsync(LoadingGrid);
+        await OnLoading();
+        var caller = DiFactory.Services.Resolve<ICallableService>();
+        caller.ThemeChangedEvent -= AppTitleBar_ThemeChangedEvent;
+        caller.ThemeChangedEvent += AppTitleBar_ThemeChangedEvent;
+        ViewModel = DiFactory.Services.Resolve<MainViewModel>();
+        navigationPage = new NavigationPage();
+        Grid.SetRow(navigationPage, 1);
+        MainGrid.Children.Add(navigationPage);
+        AppTitleBar.IsBackButtonVisible = true;
+        AppTitleBar.IsPaneButtonVisible = true;
+        AppTitleBar.IsHistoryButtonVisible = true;
+        AppTitleBar.PaneButtonClick += navigationPage.AppTitleBar_OnPaneButtonClick;
+        AppTitleBar.BackButtonClick += navigationPage.AppTitleBar_BackButtonClick;
+        SuggestBox.Visibility = Visibility.Visible;
+        NavigateHelper.ShadowNavigate(firstUri);
+        await AnimationBuilder.Create()
+            .Opacity(
+                from: 1.0,
+                to: 0,
+                duration: TimeSpan.FromSeconds(0.5))
+            .StartAsync(LoadingGrid);
+        LoadingGrid.Visibility = Visibility.Collapsed;
+    }
 
+    private async Task OnLoading()
+    {
+        ApplicationExtensionHost.Initialize(Application.Current);
+        // await Task.Delay(5000); // 测试用
+        Debug.WriteLine("123123");
+        // 配置文件
+        CoreSettings.Init();
+        InitDi();
+        // 数据库
+        InitDatabase();
+        // 插件依赖注入
+
+        var pluginServices = DiFactory.Services.Resolve<PluginLoader>();
+
+        var currentCulture = CultureInfo.CurrentUICulture;
+
+        try
+        {
+            pluginServices.Import(typeof(LocalPlugin));
+            pluginServices.Import(typeof(PluginManagerPlugin));
+            await pluginServices.ImportFromDirAsync(CoreSettings.PluginsPath);
+#if DEBUG
+            // 这里是测试插件用的, ImportFromPathAsync里填入你Debug出来的插件dll的文件夹位置
+            // await pluginServices.ImportFromPathAsync(@"C:\Users\15854\Documents\GitHub\ShadowViewer.Plugin.Bika\ShadowViewer.Plugin.Bika\bin\Debug\");
+#endif
+        }
+        catch (Exception ex)
+        {
+            Log.Error("{E}", ex);
+        }
+    }
+
+    private static void InitDi()
+    {
+        DiHelper.Init();
+        DiFactory.Services.Register<INotifyService, NotifyService>(reuse: Reuse.Singleton);
+        DiFactory.Services.Register<ICallableService, CallableService>(reuse: Reuse.Singleton);
+
+        DiFactory.Services.Register<MainViewModel>(reuse: Reuse.Singleton);
+        DiFactory.Services.Register<SettingsViewModel>(reuse: Reuse.Singleton);
+        DiFactory.Services.Register<NavigationViewModel>(reuse: Reuse.Singleton);
+    }
+
+    /// <summary>
+    /// 初始化数据库
+    /// </summary>
+    private static void InitDatabase()
+    {
+        SnowFlakeSingle.WorkId = 4;
+        var db = DiFactory.Services.Resolve<ISqlSugarClient>();
+        db.DbMaintenance.CreateDatabase();
+        db.CodeFirst.InitTables<LocalEpisode>();
+        db.CodeFirst.InitTables<LocalPicture>();
+        db.CodeFirst.InitTables<LocalTag>();
+        db.CodeFirst.InitTables<CacheImg>();
+        db.CodeFirst.InitTables<CacheZip>();
     }
 }
